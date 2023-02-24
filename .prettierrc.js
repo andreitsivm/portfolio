cb17c96cdde399bcbb0cdbc0e8362fdc1f05244d
Mykhailo Andreitsiv <andreitsivdev@gmail.com>
2023-02-24 22:49:03 +0200

    feat: setup monorepo, nextjs app

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+};
