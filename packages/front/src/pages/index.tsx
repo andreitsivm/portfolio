260a1abc8d62a720d3aca7ef2989ea5550accf04
Mykhailo Andreitsiv <andreitsivdev@gmail.com>
2023-02-26 21:42:30 +0200

    feat: {task}store, reducers, redux-toolkit

--- packages/front/src/pages/index.tsx
+++ packages/front/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import { useGetPokemonByNameQuery } from '@/services/pokemon';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
   return (
     <>
       <main>
